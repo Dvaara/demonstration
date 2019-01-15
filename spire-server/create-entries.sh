6275ab8c45d96f7675855c24b8eea01fd69b6570
Pushpalanka <lanka@wso2.com>
2019-01-15 14:48:17 +0530

    Cleanup.

--- spire-server/create-entries.sh
+++ spire-server/create-entries.sh
@@ -6,9 +6,3 @@ set -x
 ./spire-server entry create -parentID spiffe://example.org/host2 -spiffeID spiffe://example.org/front-end2 -selector unix:uid:1001 -ttl 120
 ./spire-server entry create -parentID spiffe://example.org/host3 -spiffeID spiffe://example.org/proxy-service -selector unix:uid:1000 -ttl 120
 
-//may be just consider the parentID as the load may be high with processID
-//selct may be on the same type of work..
-// starter of the process , load balancer or the own process that spawn processes different places where new processes can be started
-inherent properties
-how to believe the claim made by the process
-
