31b25ea833267865f62a0ac0b1f91a7d9c74e141
Pushpalanka <lanka@wso2.com>
2019-04-10 08:52:05 +0530

    Improvements done.

--- front-end/application/employees-token.py
+++ front-end/application/employees-token.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 opa_url = os.environ.get("OPA_ADDR", "http://localhost:8181")
 policy_path = os.environ.get("POLICY_PATH", "/v1/data/httpapi/authz")
 wso2_is = os.environ.get("WSO2_IS, wso2is")
-salaries_endpoint = "http://backend:5003/finance/salary/bob"
+salaries_endpoint = "http://backend:5003/"
 
 def get_token(scopes):
     logging.info("Requesting an oauth2 token...")
@@ -35,21 +35,26 @@ def get_token(scopes):
     return j
 
 
-def call_salary_api(token):
-    resp = requests.get(salaries_endpoint, headers={"Authorization": "Bearer "+ token})
-    if resp.status_code != 200:
-        # This means something went wrong.
-        logging.info("failed :" + resp)
-        return "FAILED"
-    return "SUCCEEDED"
+def call_salary_api(token, path, method):
+    if method == 'GET':
+        resp = requests.get(salaries_endpoint+path, headers={"Authorization": "Bearer "+ token})
+    elif method == 'POST':
+        resp = requests.post(salaries_endpoint+path, data=json.dumps({"post":"dvaara"}), headers={"Authorization": "Bearer "+ token})
+    else:
+        resp = requests.delete(salaries_endpoint+path, headers={"Authorization": "Bearer "+ token})
+
+    if resp.status_code == 200:
+        # This means access granted.
+        return "SUCCEEDED"
+    return "FAILED"
 
 
 @app.route('/', defaults={'path': ''}, methods = ['GET', 'POST', 'DELETE'])
-@app.route('/<path:path>', methods = ['GET', 'POST'])
+@app.route('/<path:path>', methods = ['GET', 'POST', 'DELETE'])
 def root(path):
     j = get_token("clearance2")
     logging.info("Token response:" + j)
-    response = call_salary_api(j)
+    response = call_salary_api(j, path,request.method)
     return render_template('index.html',name=response)
 
 if __name__ == "__main__":
